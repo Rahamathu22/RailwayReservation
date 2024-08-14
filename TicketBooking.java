33672cbdf98fde9d201e348bc008f5236f8c1a11
Rahamath22 <rahamath22104@gmail.com>
2024-08-14 12:14:06 +0530

    Some more

---
+++ TicketBooking.java
@@ -0,0 +1,35 @@
+public class TicketBooking {
+    private char source;
+    private char destination;
+    private int seats;
+    private TicketSystem ticketSystem;
+    TicketBooking(char source,char destination,int seats){
+        this.source=source;
+        this.destination=destination;
+        this.seats=seats;
+        this.ticketSystem=ticketSystem;
+    }
+
+    private void bookTicket(){
+        if(ticketSystem.checkSeatsAvailability(source, destination, seats)){
+            Ticket ticket=new Ticket(source, destination, seats, TicketStatus.Booked);
+            int newPnr=ticket.getPnrNumber();
+            ticketSystem.addToBookedTickets(newPnr, ticket);
+            System.out.println("Ticket Booked! Your PNR Number is:"+newPnr);
+            ticketSystem.decreaseSeatAvailability(source, destination, seats);
+        }
+        else{
+            if(ticketSystem.seatsBooked+seats>2){
+                System.out.println("No Tickets Available");
+            }
+            else{
+                WaitingListManager waitingListManager=new WaitingListManager();
+                waitingListManager.waitingListEntry(source,destination,seats);
+            }
+        }
+    }
+
+    protected void execute(){
+        this.bookTicket();
+    }
+}
---
+++ TicketCanceling.java
@@ -0,0 +1,37 @@
+public class TicketCanceling {
+    private int pnr;
+    private int seats;
+    private TicketSystem ticketSystem;
+    TicketCanceling(int pnr,int seats,TicketSystem ticketSystem){
+        this.pnr=pnr;
+        this.seats=seats;
+        this.ticketSystem=ticketSystem;
+    }
+
+    private void cancelTicket(){
+        Ticket ticket=ticketSystem.getTicket(pnr);
+        if(ticket!=null){
+            int bookedSeats=ticket.getSource();
+            char source=ticket.getSource(),destination=ticket.getDestination();
+            if(bookedSeats>seats){
+                ticket.setSeats(bookedSeats-seats);
+                ticketSystem.storePartiallyCanceledSeats(pnr, seats);
+                System.out.println("Partially cancelled Pnr:"+pnr);
+            }
+            else{
+                ticketSystem.processCancellation(pnr, ticket);
+                System.out.println("Cancelled Ticket pnr:"+pnr);
+            }
+            ticketSystem.increaseSeatAvailability(source, destination, seats);
+            WaitingListManager waitingListManager=new WaitingListManager();
+            waitingListManager.processWaitingList();
+        }
+        else{
+            System.out.println("Ticket with pnr "+pnr+" not found");
+        }
+    }
+
+    protected void execute(){
+        this.cancelTicket();
+    }
+}
